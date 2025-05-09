f0182f4ee7fe6f9846b699e7efe0127874ec84c1
Kelly Stuard <kellystuard@gmail.com>
2025-05-09 22:22:23 +0000

    Change to VNET and application gateway as ingress controller

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
+# Azure Provider
 provider "azurerm" {
   features {}
 }
 
+# Kubernetes Provider
 provider "kubernetes" {
   host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
   client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
@@ -9,11 +11,47 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
 }
 
+# Helm Provider
+provider "helm" {
+  kubernetes {
+    host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+  }
+}
+
+# Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "aks-resource-group"
   location = "East US"
 }
 
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "aks-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Add a new subnet for the Application Gateway
+resource "azurerm_subnet" "app_gateway_subnet" {
+  name                 = "app-gateway-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/29"] # Smallest subnet for Application Gateway
+}
+
+# AKS Cluster
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks-cluster"
   location            = azurerm_resource_group.rg.location
@@ -21,16 +59,30 @@ resource "azurerm_kubernetes_cluster" "aks" {
   dns_prefix          = "aksdns"
 
   default_node_pool {
-    name       = "default"
-    node_count = 2
-    vm_size    = "Standard_DS2_v2"
+    name           = "default"
+    node_count     = 2
+    vm_size        = "Standard_DS2_v2"
+    vnet_subnet_id = azurerm_subnet.subnet.id
   }
 
   identity {
     type = "SystemAssigned"
   }
+
+  network_profile {
+    network_plugin = "azure"       # Use Azure CNI
+    network_policy = "azure"       # Optional: Use Azure network policy
+    service_cidr   = "10.1.0.0/16" # Non-overlapping service CIDR
+    dns_service_ip = "10.1.0.10"   # DNS service IP within the service CIDR
+  }
+
+  ingress_application_gateway {
+    gateway_name = "gateway"
+    subnet_id    = azurerm_subnet.app_gateway_subnet.id
+  }
 }
 
+# Example Kubernetes Namespace
 resource "kubernetes_namespace" "example" {
   metadata {
     name = "example-namespace"
